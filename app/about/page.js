d7cb062f438a4fecac3573f1a9f9178ba34eeb68
Tyra Nyambura <tyranyamburaa@gmail.com>
2026-06-17 17:13:52 +0300

    My finished fan page

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>Harr!! Harr!🦉</h1>
+      <p>Thank you for coming out alive after viewing Taycan's fanpage🎃. Tarr Tarr👋🏽</p>
+    </main>
+  );
+}
